604336c021e70e3ed1d36fd93f7efac7fd74c458
Pradeep <pradeepkalasagond70@gmail.com>
2026-09-15 23:02:29 +0530

    Fix Sales KPI display

--- pages/sales.py
+++ pages/sales.py
@@ -162,7 +162,7 @@ st.markdown(
 
     .metric-value {
         color: #0F172A;
-        font-size: 25px;
+        font-size: 22px;
         font-weight: 850;
         line-height: 1.1;
         margin-top: 10px;
